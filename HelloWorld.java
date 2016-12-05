c94412064018d92f6227db2fb2f3ef4aee12b57b
cmadhushan <chathura.mallawaarachchi@gmail.com>
2016-12-05 15:26:42 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,11 +5,6 @@ public class HelloWorld
 		System.out.println("Hello");
 	}
 	
-	private void run()
-	{
-		System.out.println("Inside the run method");
-	}
-	
 	private void interest()
 	{
 		System.out.println("This method calculates the interest");
